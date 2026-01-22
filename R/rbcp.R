d5a10690234972ebc685e867dbb40380fed5779e
rjbgoudie <rjbgoudie@users.noreply.github.com>
2026-01-22 21:11:28 +0000

    Improve package documentation

---
+++ R/rbcp.R
@@ -0,0 +1,16 @@
+#' rbcp: A package for high-performance bulk inserts into SQL Server
+#'
+#' The `rbcp` package provides a simple and efficient way to perform bulk
+#' inserts into Microsoft SQL Server. It leverages the command-line utility
+#' `bcp`, which is designed for high-performance data loading.
+#'
+#' The primary function in this package is \code{\link{bcp_insert}}. This function
+#' handles the entire process of connecting to the database, preparing the data,
+#' and executing the bulk insert.
+#'
+#' @seealso
+#' \code{\link{bcp_insert}} for the main function of the package.
+#'
+#' @keywords internal
+#' @name rbcp
+NULL
